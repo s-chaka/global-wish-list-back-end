1974f9d79fb526e2481591eb818558a178a3695b
Selam Chaka <selblessed@gmail.com>
2023-02-09 12:42:02 -0500

    updating procfile

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ CORS(app)
 
 password = os.environ.get("MONGODB_PWD")
 connection_string = f'mongodb+srv://sdatabase:{password}@mydb.6b0j3ai.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(connection_string)
+client = MongoClient(connection_string, tls=True, tlsAllowInvalidCertificates=True)
 
 db = client.global_wish_list
 users = db.users_collection
